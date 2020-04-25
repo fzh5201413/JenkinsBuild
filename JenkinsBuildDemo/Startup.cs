2b7d6ab1e12858b42cef1a5af72d23cad0e8f677
roofs <hbfzhua@163.com>
2020-04-25 17:59:54 +0800

    fix

--- JenkinsBuildDemo/Startup.cs
+++ JenkinsBuildDemo/Startup.cs
@@ -69,10 +69,10 @@ namespace JenkinsBuildDemo
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseIpRateLimiting();
+
+            app.UseStaticFiles();
 
             app.UseRouting();
 
